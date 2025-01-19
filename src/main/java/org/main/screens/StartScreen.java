2b91ae54f43a08859d673b9cd3e7e760b979369a
cEater77 <gerbeltackerman@gmail.com>
2025-01-19 19:39:11 +0100

    startscreen kurzer fix

--- src/main/java/org/main/screens/StartScreen.java
+++ src/main/java/org/main/screens/StartScreen.java
@@ -191,4 +191,24 @@ public class StartScreen implements IScreen {
     public void resize(int width, int height) {
 
     }
+
+    @Override
+    public void onEntrance() {
+
+    }
+
+    @Override
+    public void onExit() {
+
+    }
+
+    @Override
+    public boolean shouldRenderBehind() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldAlwaysRender() {
+        return false;
+    }
 }
